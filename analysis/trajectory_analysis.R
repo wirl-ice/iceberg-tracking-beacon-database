844d88eb218f5f8e92de342d39f6381d37922649
Adam <adam.garbo@gmail.com>
2020-07-23 19:09:19 -0400

    Update trajectory_analysis.R

--- analysis/trajectory_analysis.R
+++ analysis/trajectory_analysis.R
@@ -39,7 +39,7 @@ coast_sf <- ne_countries(scale = 10, type = "countries",
                          country = c('canada','greenland'), returnclass = "sf")
 
 # Read database
-data <- read_csv("~/Desktop/cis_iceberg_beacon_database/csv/database_20200704.csv", 
+data <- read_csv("~/Desktop/cis_iceberg_beacon_database/output_data/csv/database_20200708.csv", 
                  col_types = cols(bp = col_double(), distance = col_double(), 
                                   gps_delay = col_double(), heading = col_double(), 
                                   latitude = col_double(), longitude = col_double(), 
@@ -50,28 +50,28 @@ data <- read_csv("~/Desktop/cis_iceberg_beacon_database/csv/database_20200704.cs
                                   ttff = col_double(), vbat = col_double()))
 
 # Subset data according to desired criteria
-data <- subset(data, (speed <= 5) & (distance <= 100000))
+#data <- subset(data, (speed <= 5) & (distance <= 100000))
 
 # Subset a specific beacon
-data <- subset(data, beacon_id == "300234060563100_2017")
+data <- subset(data, beacon_id == "2011_300234010958690_PII-A")
 
 # Subset by multiple beacon_id 
-data <- subset(data, (beacon_id %in% c("300434063418130_2018",
-                                       "300434063415110_2018",
-                                       "300434063419120_2018",
-                                       "300434063411050_2018",
-                                       "300434063415160_2018",
-                                       "300434063416060_2018",
-                                       "300234063265700_2019",
-                                       "300234065254740_2019",
-                                       "300434063496100_2019",
-                                       "300434063392350_2019",
-                                       "300434063292950_2019",
-                                       "300434063498160_2019",
-                                       "300434063494100_2019",
-                                       "300434063392070_2019",
-                                       "300434063394110_2019",
-                                       "300434063495310_2019")))
+data <- subset(data, (beacon_id %in% c("2018_300434063418130",
+                                       "2018_300434063415110",
+                                       "2018_300434063419120",
+                                       "2018_300434063411050",
+                                       "2018_300434063415160",
+                                       "2018_300434063416060",
+                                       "2019_300234063265700",
+                                       "2019_300234065254740",
+                                       "2019_300434063496100",
+                                       "2019_300434063392350",
+                                       "2019_300434063292950",
+                                       "2019_300434063498160",
+                                       "2019_300434063494100",
+                                       "2019_300434063392070",
+                                       "2019_300434063394110",
+                                       "2019_300434063495310")))
 
 # Subset data according to desired criteria
 data_subset <- subset(data, (latitude >= 50 & latitude <= 85) & (longitude >= -90 & longitude <= -40))
@@ -80,16 +80,11 @@ data_subset <- subset(data, (latitude >= 50 & latitude <= 85) & (longitude >= -9
 # Loop
 # ------------------------------------------------------------------------------
 
-i = 0
-
 # Execute loop for each unique beacon ID
-for (id in unique(data$beacon_id)) {
-  
-  # Increment counter
-  i = i + 1
+for (beacon in unique(data$beacon_id)) {
   
   # Subset by beacon_id
-  data_subset <- subset(data, beacon_id == id)
+  data_subset <- subset(data, beacon_id == beacon)
   
   # 1 - Convert data to simple feature (sf)
   #points_sf <- st_as_sf(data, coords = c("longitude", "latitude"), crs = 4326)
@@ -108,7 +103,7 @@ for (id in unique(data$beacon_id)) {
     geom_sf(data = points_sf, size = 1, shape = 21, colour = palette[1]) + 
     coord_sf(xlim = c(limits["xmin"], limits["xmax"]), 
              ylim = c(limits["ymin"], limits["ymax"])) +
-    labs(caption = paste(points_sf$beacon_type[1], " ", id, "\n",
+    labs(caption = paste(points_sf$beacon_type[1], " ", beacon, "\n",
                          min(points_sf$datetime_data), " to ",
                          max(points_sf$datetime_data), "\n",
                          round(max(points_sf$datetime_data) - min(points_sf$datetime_data)), " days / ",
@@ -150,15 +145,16 @@ for (id in unique(data$beacon_id)) {
   # Nested plot grids
   # -----------------------------------------------------------------------------
   
-  left <- plot_grid(map)
+  #left <- plot_grid(map)
   
-  right <- plot_grid(p1, p2, p3, ncol = 1, align = "hv")
+  #right <- plot_grid(p1, p2, p3, ncol = 1, align = "hv")
   
   # Plot grid
-  combined <- plot_grid(left, right)
+  #combined <- plot_grid(left, right)
+  combined <- plot_grid(map)
   
   # Save plot
-  save_plot(filename = paste(i, "_", points_sf$beacon_id[1], ".png", sep = ""), 
+  save_plot(filename = paste(beacon, ".png", sep = ""), 
             plot = combined,
             base_width = 12,
             base_height = 7,
@@ -217,10 +213,10 @@ sprintf(buffer, "You have %2d minutes left.", min);
 #geom_line(aes(datetime_data, distance/1000), colour = palette[3]) +
 
 # Subset by beacon_id
-data_subset <- subset(data, beacon_id == ("300434063415110_2018"))
-data_subset <- subset(data, beacon_id == ("300434063418130_2018"))
-data_subset <- subset(data, beacon_id == ("300434063415160_2018"))
-data_subset <- subset(data, beacon_id == ("300234063265700_2019"))
+data_subset <- subset(data, beacon_id == ("2018_300434063415110"))
+data_subset <- subset(data, beacon_id == ("2018_300434063418130"))
+data_subset <- subset(data, beacon_id == ("2018_300434063415160"))
+data_subset <- subset(data, beacon_id == ("2018_300234063265700"))
 
 sprintf("%.png", points_sf$beacon_id[1])
 ggsave(filename = paste(points_sf$beacon_id[1], ".png", sep = ""), 
